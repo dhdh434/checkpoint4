a94f3de9356bbb28cf2152592c4c3f36274353e4
David <davidshunter2@gmail.com>
2023-01-30 11:45:33 -0700

    final

--- Building first Web App/wwwroot/js/fanmail.js
+++ Building first Web App/wwwroot/js/fanmail.js
@@ -4,10 +4,43 @@
 ////        document.getElementById("txtmess").value);
 ////})
 
-$("#btnsend").click( function () {
-    alert("From: " + $("#frm").val() + ", Subject: " +
-        $("#sbj").val() + ", Message: " +
-        $("#txtmess").val());
+//create a function to calculate the grade
+$("#btnsend").click(function () {
+    var grade;
+    grade = ((parseFloat($("#as").val())) * 0.5) + ((parseFloat($("#gp").val())) * 0.1) +
+        ((parseFloat($("#q").val())) * 0.1) + ((parseFloat($("#me").val())) * 0.1) +
+        ((parseFloat($("#gp").val())) * 0.1) + ((parseFloat($("#gp").val())) * 0.1);
+    grade = grade.toFixed(2);
 
-    $("#pic").fadeOut();
+    //assign the correct grade letter to the correct score
+    var gradeLetter = "";
+    if (grade >= 0.94) {
+        gradeLetter = "A";
+    } else if (grade >= 0.9) {
+        gradeLetter = "A-";
+    } else if (grade >= 0.87) {
+        gradeLetter = "B+";
+    } else if (grade >= 0.84) {
+        gradeLetter = "B";
+    } else if (grade >= 0.8) {
+        gradeLetter = "B-";
+    } else if (grade >= 0.77) {
+        gradeLetter = "C+";
+    } else if (grade >= 0.74) {
+        gradeLetter = "C";
+    } else if (grade >= 0.7) {
+        gradeLetter = "C-";
+    } else if (grade >= 0.67) {
+        gradeLetter = "D+";
+    } else if (grade >= 0.64) {
+        gradeLetter = "D";
+    } else if (grade >= 0.6) {
+        gradeLetter = "D-";
+    } else {
+        gradeLetter = "E";
+    }
+
+    //output
+    output.innerHTML = gradeLetter;
+
 })
