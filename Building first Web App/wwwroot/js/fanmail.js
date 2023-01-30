14a3aa7b41a46a2adca5f784f58a5d9eadcf7a59
David <davidshunter2@gmail.com>
2023-01-30 10:12:39 -0700

    Add project files.

---
+++ Building first Web App/wwwroot/js/fanmail.js
@@ -0,0 +1,13 @@
+﻿////document.getElementById("btnsend").addEventListener("click", function () {
+////    alert("From: " + document.getElementById("frm").value + ", Subject: " +
+////        document.getElementById("sbj").value + ", Message: " +
+////        document.getElementById("txtmess").value);
+////})
+
+$("#btnsend").click( function () {
+    alert("From: " + $("#frm").val() + ", Subject: " +
+        $("#sbj").val() + ", Message: " +
+        $("#txtmess").val());
+
+    $("#pic").fadeOut();
+})
