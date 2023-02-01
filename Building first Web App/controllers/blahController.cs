6c76db76baaea87e9e224a854ad805faa5d9c297
David <davidshunter2@gmail.com>
2023-01-31 17:39:44 -0700

    branch5

---
+++ Building first Web App/controllers/blahController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Building_first_Web_App.controllers
+{
+    public class blahController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
